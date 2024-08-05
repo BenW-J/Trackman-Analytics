9e273e26a21cb6acb43d731bcd8e4fae374dfec1
benny0jamin <bwagjor@gmail.com>
2024-08-05 17:19:48 +1000

    tutorial

--- Supabase Tutorial/main.py
+++ Supabase Tutorial/main.py
@@ -3,12 +3,24 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from supabase import create_client, Client
+from datetime import datetime, timedelta
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
 
 
-response = supabase.table("todos").select("id,name").eq("name","Item 1").execute()
+# response = supabase.table("todos").select("id,name").eq("name","Item 1").execute()
+
+
+# print(response)
+
+created_at = datetime.utcnow() - timedelta(hours=2)
+
+response = (
+    supabase.table("todos").insert({"name": "Todo 3","created_at":str(created_at)}).execute()
+)
+
+response = supabase.table("todos").select("*").execute()
 
 print(response)
